d8654973478e2dcc5b99cf67870bbb0edd4bfc57
James Stephaniuk <jstephaniuk@gmail.com>
2019-05-30 20:38:53 -0700

    Make navbar better

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -7,13 +7,16 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+      <li class="nav-item {{ Request::is('/') ? "active": ""}}">
+        <a class="nav-link" href="/">Home<span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
+       <li class="nav-item {{ Request::is('blog') ? "active": ""}}">
+        <a class="nav-link" href="/blog">Blog<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item {{ Request::is('about') ? "active": ""}}">
         <a class="nav-link" href="/about">About</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item {{ Request::is('contact') ? "active": ""}}">
         <a class="nav-link" href="/contact">Contact</a>
       </li>
       <ul class="navbar-nav ml-auto">
@@ -22,7 +25,7 @@
             Account
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">My Account</a>
+            <a class="dropdown-item" href="{{ route('posts.index')}}">Posts</a>
             <div class="dropdown-divider"></div>
             <a class="dropdown-item" href="#">Logout</a>
           </div>
